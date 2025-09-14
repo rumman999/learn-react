1c3de01710818f456e82219e961a181d78fb84bd
rumman999 <rumman.rk17@gmail.com>
2025-09-15 00:06:22 +0600

    Improve multiselection

--- react-projects-freecodecamp/react-projects/src/components/accordion/index.jsx
+++ react-projects-freecodecamp/react-projects/src/components/accordion/index.jsx
@@ -1,60 +1,77 @@
-import React, { useState } from 'react'
-import data from './data.js'
-import './styles.css'
-
+import React, { useState } from "react";
+import data from "./data.js";
+import "./styles.css";
 
 function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setenableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
-  const [selected, setSelected] = useState("");
-  const [enableMultiSelection, setenableMultiSelection] = useState(false)
-  const [multiple, setMultiple] = useState([])
-
-  function handleSingleSelection(id){
-    if(selected === id){
-      setSelected(null)
-    } else{
-      setSelected(id)
+  function handleSingleSelection(id) {
+    if (selected === id) {
+      setSelected(null);
+    } else {
+      setSelected(id);
     }
   }
 
-  function handleMultiSelection(id){
-    let copyMultiple = [...multiple]
+  function handleMultiSelection(id) {
+    let copyMultiple = [...multiple];
 
-    const findIndex = copyMultiple.indexOf(id)
+    const findIndex = copyMultiple.indexOf(id);
 
-    if(findIndex === -1){
-      copyMultiple.push(id)
-    } else{
-      copyMultiple.splice(findIndex, 1)
+    if (findIndex === -1) {
+      copyMultiple.push(id);
+    } else {
+      copyMultiple.splice(findIndex, 1);
     }
 
-    setMultiple(copyMultiple)
-
+    setMultiple(copyMultiple);
   }
 
-  function handleMultiselection(){
-    setenableMultiSelection(!enableMultiSelection)
+  function handleMultiselection() {
+    setenableMultiSelection(!enableMultiSelection);
+    if (enableMultiSelection === true) {
+      setSelected(null);
+      setMultiple([]);
+    }
   }
 
   return (
     <div className="wrapper">
-      <button onClick={handleMultiselection}>Enable/Disable Multiselection</button>
+      <button onClick={handleMultiselection}>
+        Enable/Disable Multiselection
+      </button>
       <div className="accordion">
-        { data && data.length > 0 ?
-          data.map(dataItem => <div className="item">
-            <div onClick={(enableMultiSelection) ? ()=> handleMultiSelection(dataItem.id) : () => handleSingleSelection(dataItem.id)} className="title">
-              <h3>{dataItem.question} </h3>
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question} </h3>
+              </div>
+              <span>+</span>
+              {(
+                enableMultiSelection
+                  ? multiple.indexOf(dataItem.id) !== -1
+                  : selected === dataItem.id
+              ) ? (
+                <p>{dataItem.answer}</p>
+              ) : null}
             </div>
-            <span>+</span>
-            {
-              selected == dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? <p>{dataItem.answer}</p> : null 
-            }
-          </div>
-          )
-        : <p>No data found.</p> }
+          ))
+        ) : (
+          <p>No data found.</p>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Accordion
+export default Accordion;
